2c4732be69e73116d026ff6892cf606189b15855
Zac Alexander <zac@znacreative.com>
2021-04-28 18:52:16 +0000

    Add http_basic_authentication_with

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: "Jungle", password: "book", except: :index
+
+
   def index
     @products = Product.order(id: :desc).all
   end
